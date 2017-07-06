36bc50b71f7accfc8c6d187d1ccfa464434b2b82
David Hunter <david@mapvine.com>
2017-07-05 21:54:06 -0600

    Add image download

--- app/components/Home.js
+++ app/components/Home.js
@@ -2,20 +2,33 @@ import React, {Component} from 'react';
 import * as T from 'prop-types';
 import {topInputOnChange, bottomInputOnChange, topInputOnDeleteChange, bottomInputOnDeleteChange} from '../actions';
 import {connect} from 'react-redux';
+import domtoimage from 'dom-to-image';
+import {saveAs} from 'file-saver';
 
 class Home extends Component {
+
+  downloadImage() {
+    domtoimage.toBlob(document.getElementById('outter'))
+      .then(function(blob) {
+        console.log('blob', blob);
+        saveAs(blob, 'mem.png');
+      });
+  }
 
   render() {
     let {topText, bottomText} = this.props;
     return (
-      <div>
-        <div className="meme">
-          <p className="top">{topText}</p>
-          <p className="bottom">{bottomText}</p>
+      <div id="this">
+        <div id="outter">
+          <div id="meme" className="meme">
+            <p className="top">{topText}</p>
+            <p className="bottom">{bottomText}</p>
+          </div>
         </div>
         <div className="centered-inputs">
-          <input placeholder="Top Text" onKeyUp={this.props.topChange.bind(this)}/>
-          <input placeholder="Bottom Text" onKeyUp={this.props.bottomChange.bind(this)}/>
+          <input placeholder="Top Text" onKeyUp={this.props.topChange.bind(this)}/><br/>
+          <input placeholder="Bottom Text" onKeyUp={this.props.bottomChange.bind(this)}/><br/>
+          <button onClick={this.downloadImage.bind(this)}>Download</button>
         </div>
       </div>
     );
@@ -26,7 +39,7 @@ const mapDispatchToProps = (dispatch) => {
   return {
     topChange: function(e) {
       var inp = String.fromCharCode(e.keyCode);
-      if(e.keyCode >= 48 && e.keyCode <= 90 || e.keyCode === 32) {
+      if (e.keyCode >= 48 && e.keyCode <= 90 || e.keyCode === 32) {
         dispatch(topInputOnChange(inp));
       } else if (e.keyCode === 8) {
         dispatch(topInputOnDeleteChange());
@@ -36,7 +49,7 @@ const mapDispatchToProps = (dispatch) => {
     },
     bottomChange: function(e) {
       var inp = String.fromCharCode(e.keyCode);
-      if(e.keyCode >= 48 && e.keyCode <= 90 || e.keyCode === 32) {
+      if (e.keyCode >= 48 && e.keyCode <= 90 || e.keyCode === 32) {
         dispatch(bottomInputOnChange(inp));
       } else if (e.keyCode === 8) {
         dispatch(bottomInputOnDeleteChange());
